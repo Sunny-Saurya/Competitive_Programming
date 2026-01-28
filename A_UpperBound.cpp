8e31e71989c377508c6603b7d24ad5a73e5e0057
Sunny Kumar <sunnysaurya9955@gmail.com>
2026-01-28 12:06:45 +0530

    Upper bound

---
+++ A_UpperBound.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int upperBound(vector<int>& arr, int target) {
+        // code here
+        int l = 0;
+        int r = arr.size() - 1;
+        int idx = arr.size();
+        
+        while(l <= r){
+            int mid = l + (r - l)/2;
+            if(arr[mid] > target){
+                idx = mid;
+                r = mid - 1;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+        return idx;
+    }
+};
