92a73aade5dd6ea91fa301bac876bdc3644ffe0d
Sunny Kumar <sunnysaurya9955@gmail.com>
2026-03-26 16:28:26 +0530

    One and Two

--- A_One_and_Two.cpp
+++ A_One_and_Two.cpp
@@ -12,29 +12,27 @@ void solve(){
     
     int countTwos = count(arr.begin(), arr.end(), 2);
 
-    if(countTwos == 0){
-        cout << 1 << endl;
-        return;
-    }
-
-
     if(countTwos % 2 != 0){
         cout << -1 << endl;
         return;
     }
-    int need = countTwos/2;
-    int cnt = 0;
-    for(int i = 0; i < n; i++){
-        if(arr[i] == 2) cnt++;
+    long long res = 1;
+    for(int num : arr){
+        res *= num;
+    }
+    long long store = res;
+    long long pro = 1;
+    for(int i = 0 ; i < n; i++){
+        pro *= arr[i];
+        res /= arr[i];
 
-        if(cnt == need){
-            cout << i + 1 << endl;
+        if(pro == res){
+            cout << i+1 << endl;
             return;
         }
     }
-
     cout << -1 << endl;
-    // return;
+    return;
 }
 
 int main() {
