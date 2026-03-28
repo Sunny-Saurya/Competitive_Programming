4e9d54297a5d5b0c559ee499faf1fdee21ea022f
Sunny Kumar <sunnysaurya9955@gmail.com>
2026-03-28 15:36:18 +0530

    Insertion

---
+++ A_Insertion.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void insertionSort(vector<int>&arr)
+{
+    int n = arr.size();
+    for (int i = 1; i < n; ++i) {
+        int key = arr[i];
+        int j = i - 1;
+        
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+int main(){
+    vector<int>arr = {64,25,12,22,11};
+    int n = arr.size();
+    for(int i = 0; i < n;i++){
+        cout << arr[i] << " ";
+    }
+    insertionSort(arr);
+    cout << "After Sorting : " << endl;
+
+    for(int i = 0; i < n;i++){
+        cout << arr[i] << " ";
+    }
+}
