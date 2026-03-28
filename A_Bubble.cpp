b48bcd4f13486d0723e639a4db6ea325901bfeea
Sunny Kumar <sunnysaurya9955@gmail.com>
2026-03-28 15:22:15 +0530

    Bubble sort

---
+++ A_Bubble.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void sortArray(vector<int>&arr){
+    int n = arr.size();
+
+    for(int i = 0; i < n-1; i++){
+        for(int j = 0; j < n - i - 1; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
+
+int main(){
+    vector<int>arr = {64,25,12,22,11};
+    int n = arr.size();
+    for(int i = 0; i < n;i++){
+        cout << arr[i] << " ";
+    }
+    sortArray(arr);
+    cout << "After Sorting : " << endl;
+
+    for(int i = 0; i < n;i++){
+        cout << arr[i] << " ";
+    }
+}
